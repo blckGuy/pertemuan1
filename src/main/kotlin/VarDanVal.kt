e97183ec9155121cfa02f1eb13f2483eeef4638c
blckguy <dni19877@gmail.com>
2022-03-10 15:00:32 +0700

    pertemuan ke 1 : memulai pemrograman dengan kotlin

---
+++ src/main/kotlin/VarDanVal.kt
@@ -0,0 +1,13 @@
+
+
+fun main(args: Array<String>) {
+    //var merupakan  sebuah variabel yang nilainya bisa diubah dan kebalikannya adalah val(konstanta)
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = $dynamicVar")
+
+//val adalah variabel yang nilainya adalah konstanta dan tidak bisa diubah
+    val maxFloat: Float = 9.123456789f
+    val maxDouble: Double = 9.123456789
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+}
